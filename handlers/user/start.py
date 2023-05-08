86922db437ca2263155133574b314adfa62b15d2
Sanjar Omonov <90836075+Omonov01@users.noreply.github.com>
2023-05-08 14:58:20 +0500

    Update start.py

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -6,7 +6,7 @@ from states.forma import userposition
 from loader import dp
 
 
-@dp.message_handler(CommandStart())
+@dp.message_handler(CommandStart(),state="*")
 async def bot_start(message: types.Message):
     await message.answer(f"Assalom alaykum, {message.from_user.full_name}!")
     await message.answer(f"Hush kelipsiz o'z shaxsiy oynangizni tanlang",reply_markup=base_menu)
